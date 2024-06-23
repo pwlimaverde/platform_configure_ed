3a20bd5343c56395848ad1068031becaa32bb906
pwlimaverde <pwlimaverde@gmail.com>
2024-06-23 13:35:58 +0000

    ajustes plataform

--- apps/painel_configure_ed/lib/main.dart
+++ apps/painel_configure_ed/lib/main.dart
@@ -1,21 +1,22 @@
+import 'package:dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
+import 'src/app_widget.dart';
 import 'src/app_widget_animation.dart';
+import 'src/utils/firebase_options.dart';
 
 void main() async {
-  // await startServices(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  //   permissionInit: false,
-  //   checkConnectInit: false,
-  //   dartPluginInit: false,
-  // );
   runApp(
     const AppWidgetAnimation(),
   );
-  
-  // final teste = await FeaturesServicePresenter.to.externalStorage.read(Registro(colecao: "user", documento: "teste"), false);
-  // Logger().i(teste);
-  // runApp(
-  //   const AppWidget(),
-  // );
+  await startServices(
+    options: DefaultFirebaseOptions.currentPlatform,
+    permissionInit: false,
+    checkConnectInit: false,
+    dartPluginInit: false,
+  );
+
+  runApp(
+    const AppWidget(),
+  );
   
 }
